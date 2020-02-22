3057865709e94de6690556732334dff7146e5a15
Kovalenko K. Y <qwertyklopan@gmail.com>
2020-02-22 17:22:20 +0000

    Problem with sql script fixed

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -5,18 +5,18 @@ DROP TABLE IF EXISTS skills;
 DROP TABLE IF EXISTS accounts;
 
 CREATE TABLE IF NOT EXISTS skills (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY NOT NULL,
     name Varchar(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS accounts (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY NOT NULL,
     name Varchar(255) NOT NULL,
     status Varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS developers (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY NOT NULL,
     first_name Varchar(255),
     last_name Varchar(255),
     account_id INT,

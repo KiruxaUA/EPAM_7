fe338f7adcfeae42421ffca55769a929409041d6
Kovalenko K. Y <qwertyklopan@gmail.com>
2020-02-22 17:08:40 +0000

    Added suggested improvements

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,18 +1,18 @@
 USE scjb6rzjixjf7taw;
-INSERT INTO skills (name) VALUES ('Java');
-INSERT INTO skills (name) VALUES ('C++');
-INSERT INTO skills (name) VALUES ('C#');
-INSERT INTO skills (name) VALUES ('Python');
+INSERT INTO skills (id, name) VALUES ('1', 'Java');
+INSERT INTO skills (id, name) VALUES ('2', 'C++');
+INSERT INTO skills (id, name) VALUES ('3', 'C#');
+INSERT INTO skills (id, name) VALUES ('4', 'Python');
 
-INSERT INTO accounts (name, status) VALUES ('Joe', 'ACTIVE');
-INSERT INTO accounts (name, status) VALUES ('William', 'DELETED');
-INSERT INTO accounts (name, status) VALUES ('John', 'BANNED');
-INSERT INTO accounts (name, status) VALUES ('James', 'ACTIVE');
+INSERT INTO accounts (id, name, status) VALUES ('1', 'Joe', 'ACTIVE');
+INSERT INTO accounts (id, name, status) VALUES ('2', 'William', 'DELETED');
+INSERT INTO accounts (id, name, status) VALUES ('3', 'John', 'BANNED');
+INSERT INTO accounts (id, name, status) VALUES ('4', 'James', 'ACTIVE');
 
-INSERT INTO developers (first_name, last_name, account_id) VALUES ('Joe', 'Williams', '1');
-INSERT INTO developers (first_name, last_name, account_id) VALUES ('William', 'Shakespear', '2');
-INSERT INTO developers (first_name, last_name, account_id) VALUES ('John', 'Higgins', '3');
-INSERT INTO developers (first_name, last_name, account_id) VALUES ('James', 'Holden', '4');
+INSERT INTO developers (id, first_name, last_name, account_id) VALUES ('1', 'Joe', 'Williams', '1');
+INSERT INTO developers (id, first_name, last_name, account_id) VALUES ('2', 'William', 'Shakespear', '2');
+INSERT INTO developers (id, first_name, last_name, account_id) VALUES ('3', 'John', 'Higgins', '3');
+INSERT INTO developers (id, first_name, last_name, account_id) VALUES ('4', 'James', 'Holden', '4');
 
 INSERT INTO developer_skill (developer_id, skill_id) VALUES ('1', '3');
 INSERT INTO developer_skill (developer_id, skill_id) VALUES ('2', '2');
